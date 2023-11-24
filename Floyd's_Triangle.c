30e86ae0db39513a49ac173e26dfef0b6bcbee75
BlacKPooL125 <151719917+BlacKPooL125@users.noreply.github.com>
2023-11-24 20:51:25 +0600

    Update Floyd's_Triangle.c

--- Floyd's_Triangle.c
+++ Floyd's_Triangle.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
-#include <math.h>
 
 int main() {
     
-    int row;
+    int rows;
 
-    printf("Enter row: ");
-    scanf("%d", &row);
-    
-    for (int i = 1; i <= row; i++)
+    printf("Enter how many rows: ");
+    scanf("%d", &rows);
+
+    for (int i = 1; i <= rows; i++)
     {
         for (int j = 1; j <= i; j++)
         {
@@ -23,6 +22,6 @@ int main() {
         }
         printf("\n");
     }
-    
+
     return 0;
 }
