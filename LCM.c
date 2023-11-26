52bf1297495e4e938e47eac4845d53e22c47474c
BlacKPooL125 <151719917+BlacKPooL125@users.noreply.github.com>
2023-11-26 11:42:33 +0600

    Update LCM.c

--- LCM.c
+++ LCM.c
@@ -1,27 +1,32 @@
 #include <stdio.h>
-#include <math.h>
 
 int main() {
     
     int num1, num2;
-    
+
     printf("Enter first number: ");
     scanf("%d", &num1);
     printf("Enter second number: ");
     scanf("%d", &num2);
 
-    int LCM;
-
-    for (int i = 1; ; i++)
+    if (num1 == num2)
     {
-        if (i % num1 == 0 && i % num2 == 0)
+        printf("The LCM of %d and %d is: %d\n", num1, num2, num1);
+    }
+    else
+    {
+        int LCM;
+        int high_num = (num1 > num2) ? num1 : num2;
+        for (int i = high_num; ; i = i + high_num)
         {
-            LCM = i;
-            break;
+            if (i % num1 == 0 && i % num2 == 0)
+            {
+                LCM = i;
+                printf("The LCM of %d and %d is: %d\n", num1, num2, LCM);
+                break;
+            }
         }
     }
-    
-    printf("The LCM of %d and %d is: %d\n", num1, num2, LCM);
     
     return 0;
 }
